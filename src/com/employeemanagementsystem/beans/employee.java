44dc669bfeeaa25b5847ad640c05886b9db2fdf6
Mukesh Kumar Dadarwal <mukesh.dadarwal@ALPPUNEDC.COM>
2021-06-23 13:05:28 +0530

    first commit

---
+++ src/com/employeemanagementsystem/beans/employee.java
@@ -0,0 +1,75 @@
+package com.employeemanagementsystem.beans;
+
+public class employee {
+	private int Id;
+	private String Name;
+	private String Contact;
+	private String Email;
+	private String Dept;
+	private int Salary;
+
+	@Override
+	public String toString() {
+		return "employee [Id=" + Id + ", Name=" + Name + ", Contact=" + Contact + ", Email=" + Email + ", Dept=" + Dept
+				+ ", Salary=" + Salary + "]";
+	}
+
+	public employee(int id, String name, String contact, String email, String dept, int salary) {
+		super();
+		Id = id;
+		Name = name;
+		Contact = contact;
+		Email = email;
+		Dept = dept;
+		Salary = salary;
+	}
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getContact() {
+		return Contact;
+	}
+
+	public void setContact(String contact) {
+		Contact = contact;
+	}
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+	public String getDept() {
+		return Dept;
+	}
+
+	public void setDept(String dept) {
+		Dept = dept;
+	}
+
+	public int getSalary() {
+		return Salary;
+	}
+
+	public void setSalary(int salary) {
+		Salary = salary;
+	}
+
+}
